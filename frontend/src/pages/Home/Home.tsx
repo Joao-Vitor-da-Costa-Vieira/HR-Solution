da01b7a73692d93e1325050e9847f8d4e56d8820
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-10-31 20:27:33 -0300

    feat: adição de alguns componentes

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,7 +1,18 @@
 import { Link } from "react-router-dom";
 import "./Home.css"
+import { PerguntaDissertativa } from "../../components/PerguntaDissertativa/PerguntaDissertativa";
+import PerguntaLikert from "../../components/PerguntaLikert/PerguntaLikert";
+import { useState, type FormEvent } from "react";
 
 export const Home = () => {
+    const [respostaDissertativa, setRespostaDissertativa] = useState("");
+    const [respostaLikert, setRespostaLikert] = useState("");
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        alert("Resposta enviada: \n1 = " + respostaDissertativa+"\n2 = "+respostaLikert);
+    };
+    
     return (
         <div>
             <h1 id="titulo">Bem vindo a página principal</h1>
@@ -11,6 +22,27 @@ export const Home = () => {
                     Ir para "Gerenciar Entidades"
                 </button>
             </Link>
+
+            <form onSubmit={handleSubmit}>
+                <PerguntaDissertativa
+                    name="pergunta-1"
+                    pergunta="Numa situação XYZ, o que você faria se ABC?"
+                    onChange={setRespostaDissertativa}
+                />
+
+                <PerguntaLikert
+                    name="pergunta-2"
+                    question='Gosto de ser o centro das atenções.'
+                    onChange={setRespostaLikert}
+                />
+
+                 <button
+                    type="submit"
+                    style={{ padding: "10px 16px", borderRadius: 6 }}
+                 >
+                    Enviar
+                </button>
+            </form>
         </div>
     );
 }
