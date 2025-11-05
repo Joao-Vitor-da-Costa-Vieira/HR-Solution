a6f76822e109ecfea1a335bead33cda8a58692f5
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-11-05 17:28:05 -0300

    feat: mais componentes para o frontend

---
+++ frontend/src/components/Navegacao/Navegacao.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import "./Navegacao.css";
+
+export function Navegacao(){
+    return (
+        <nav className="navegacao">
+            <Link to={"/"}>
+                • Home
+            </Link>
+
+            <Link to={"/crudSimples"}>
+                • Crud Simples
+            </Link>
+
+            <Link to={"/questionario"}>
+                • Questionário
+            </Link>
+
+            <Link to={"/analise"}>
+                • Análise Questionário
+            </Link>
+
+            <Link to={"/testesLoucos"}>
+                • Testes Loucos
+            </Link>
+        </nav>
+    );
+}
