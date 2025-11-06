49e8ac51a4c79b84db1aacdc5778b90154962e33
João Vitor da Costa Vieira <jvcvieira2001@hotmail.com>
2025-11-06 15:39:34 -0300

    feat: adição de atributos para entidade Analise

--- backend/src/main/java/com/hr_solution/model/entities/Analise.java
+++ backend/src/main/java/com/hr_solution/model/entities/Analise.java
@@ -15,5 +15,46 @@ public class Analise {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    //Eixos do Perfil
+
+    //Máximo atingível no eixo da Atitude
+    private Integer EixoMaxAtitude;
+
+    private Integer EixoMaxCapacidade;
+
+    //Eixos da Liderança
+
+    //Máximo atingível no eixo da Liderança Comandante
+    private Integer LiderancaComandanteTotal;
+
+    private Integer LiderancaTreinadorTotal;
     
+    private Integer LiderancaOrientadorTotal;
+
+    private Integer LiderancaDesafiadorTotal;
+
+    //Eixos do Perfil
+
+    //Valor atingido nos eixos de Perfil
+    private Integer eixoAtitude;
+
+    private Integer eixoCapacidade;
+
+    //Eixos da Liderança
+
+    //Valor atingido nos eixos de Liderança
+    private Integer eixoLiderancaComandante;
+
+    private Integer eixoLiderancaTreinador;
+    
+    private Integer eixoLiderancaOrientador;
+
+    private Integer eixoLiderancaDesafiador;
+
+    private String resumoIA;
+
+    @OneToOne
+    @JoinColumn(name = "resposta_avaliacao_id")
+    private RespostaAvaliacao respostaAvaliacao;
 }
